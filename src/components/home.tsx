896d3016b526d9ffdf855742e60c884d6bc5369a
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2022-03-23 17:04:53 +0200

    Add files via upload

---
+++ src/components/home.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import Footer from "./microcomponents/footer";
+import Header from "./microcomponents/header";
+import Wheel from "./microcomponents/wheel";
+
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <div className="container">
+      <Header></Header>
+
+      <div className="rowContainer">
+        <span>
+          <h1>Because weather is not a matter of luck.</h1>
+          <p>
+            Login so you can have access to detailed weather informations.
+            <br />
+            Aeolus is a project just for web development practise.
+            <br />
+            <span className="bold"> Frequently asked questions:</span> <br />
+            Which api is used to fetch weather data? <br />
+            <span className="bold"> MetaweatherApi: </span>
+            <a href="https://en.wikipedia.org/wiki/Aeolus">
+              MetaweatherApi
+            </a>{" "}
+            <br />
+            How you came up with this name? <br />
+            Read this :{" "}
+            <a
+              href="https://en.wikipedia.org/wiki/Aeolus"
+              data-testid="link-aeolus"
+            >
+              Aeolus
+            </a>
+          </p>
+        </span>
+        {/* <Wheel></Wheel> */}
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Home;
