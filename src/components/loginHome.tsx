3c1bd8aad9ddcf7dad147daa571a4f0eff620788
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2022-03-26 19:39:08 +0200

    Add files via upload

--- src/components/loginHome.tsx
+++ src/components/loginHome.tsx
@@ -7,11 +7,19 @@ import { useHistory } from "react-router-dom";
 import Header from "./microcomponents/header";
 import LocationList from "./microcomponents/locationList";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { GrClose } from "react-icons/gr";
+import { IoMdClose } from "react-icons/io";
 import { InputContext } from "../context/inputContext";
+import { DisplayListContext } from "../context/displayListContext";
+import { motion } from "framer-motion";
 
 const LoginHome: FC = () => {
   const history = useHistory();
+  const variants = {
+    open: { opacity: 1, x: 0 },
+    closed: { opacity: 0, x: "-100%" },
+  };
+  const [isOpen, setIsOpen] = useState(false);
+
   const { userData, setUserData } = useContext(UserContext);
   const [displayList, setDisplayList] = useState<boolean>(false);
   const [inputData, setInputData] = useState<any>({
@@ -34,24 +42,27 @@ const LoginHome: FC = () => {
   return (
     <div className="container">
       <Header></Header>
-      <GiHamburgerMenu
-        size={30}
+      <motion.button
+        className="buttonToLocationList"
         onClick={() => setDisplayList(!displayList)}
-        className={
-          displayList ? "buttonToLocationListAnimated" : "buttonToLocationList"
-        }
-      ></GiHamburgerMenu>
-      <GrClose
-        className={
-          displayList
-            ? "buttonToLocationListCloseAnimated"
-            : "buttonToLocationListClose"
-        }
-        size={30}
-        onClick={() => setDisplayList(!displayList)}
-      ></GrClose>
+        whileHover={{ scale: 1.4 }}
+        whileTap={{ scale: 0.7 }}
+        animate={{
+          borderRadius: displayList ? "25%" : "50%",
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 40,
+        }}
+      >
+        {displayList ? <IoMdClose size={35} /> : <GiHamburgerMenu size={30} />}
+      </motion.button>
       <InputContext.Provider value={{ inputData, setInputData }}>
-        {displayList && <LocationList></LocationList>}
+        {displayList && (
+          <DisplayListContext.Provider value={{ displayList, setDisplayList }}>
+            <LocationList />
+          </DisplayListContext.Provider>
+        )}
         <SearchBar></SearchBar>
       </InputContext.Provider>
       <Footer></Footer>
