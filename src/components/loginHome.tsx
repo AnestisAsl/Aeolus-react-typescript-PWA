c49315881da1d7afac58ddeae2c7c89ab64c407d
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2022-04-11 17:07:00 +0300

    Add files via upload

--- src/components/loginHome.tsx
+++ src/components/loginHome.tsx
@@ -1,24 +1,19 @@
 import * as React from "react";
-import SearchBar from "./microcomponents/searchbar";
-import Footer from "./microcomponents/footer";
 import { useContext, FC, useEffect, useState } from "react";
 import { UserContext } from "../context/userContext";
 import { useHistory } from "react-router-dom";
-import Header from "./microcomponents/header";
-import LocationList from "./microcomponents/locationList";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
 import { InputContext } from "../context/inputContext";
 import { DisplayListContext } from "../context/displayListContext";
 import { motion } from "framer-motion";
+import Header from "./microcomponents/header";
+import LocationList from "./microcomponents/locationList";
+import SearchBar from "./microcomponents/searchbar";
+import Footer from "./microcomponents/footer";
 
 const LoginHome: FC = () => {
   const history = useHistory();
-  const variants = {
-    open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: "-100%" },
-  };
-  const [isOpen, setIsOpen] = useState(false);
 
   const { userData, setUserData } = useContext(UserContext);
   const [displayList, setDisplayList] = useState<boolean>(false);
@@ -28,14 +23,14 @@ const LoginHome: FC = () => {
   });
   useEffect(() => {
     var userObjFromLocalStorage: any = localStorage.getItem("auth-user");
-    var tokenFromLocalStorage: any = localStorage.getItem("auth-token");
+    var tokenFromLocalStorage: any = localStorage.getItem("x-auth-token");
     if (userData.token === undefined) {
       setUserData({
         token: tokenFromLocalStorage,
         user: JSON.parse(userObjFromLocalStorage),
       });
     }
-    if (!localStorage.getItem("auth-token")) {
+    if (!localStorage.getItem("x-auth-token")) {
       history.push("/");
     }
   }, [userData]);
